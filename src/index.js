9820fd2e40addcbd7688dd9d04960906db840e15
Chris Chuck <chrischuck@Chriss-MacBook-Pro.local>
2021-02-12 22:15:37 -0800

    Use fc

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-  render() {
-    return <div>Hello world!</div>;
-  }
-}
+const App = () => {
+  return <div>Hello world!</div>;
+};
 
 ReactDOM.render(<App />, document.getElementById('app'));
